34de0d89365d60dafa6a96ec8230955fc25af5b1
OJP98 <ojuarez_p@hotmail.com>
2021-07-19 18:47:55 -0600

    Initial commit - main features implemented

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainScript : MonoBehaviour
+{
+    public Metronome metronome;
+    public InputField inputField; 
+
+    public void HandleDropDownInputData(int val)
+    {
+        switch (val)
+        {
+            case 0:
+                metronome.Metric = 2;
+                break;
+
+            case 1:
+                metronome.Metric = 3;
+                break;
+
+            case 2:
+                metronome.Metric = 4;
+                break;
+
+            case 3:
+                metronome.Metric = 5;
+                break;
+
+            case 4:
+                metronome.Metric = 6;
+                break;
+
+            case 5:
+                metronome.Metric = 7;
+                break;
+
+            default:
+                metronome.Metric = 4;
+                break;
+        }
+    }
+
+    public void HandleInputData(int value)
+    {
+        int inputValue = Convert.ToInt32(inputField.text);
+        metronome.BPM = inputValue;
+    }
+}
---
+++ Assets/Scripts/Metronome.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metronome : MonoBehaviour
+{
+    public double bpm = 120.0F;
+    public double bpmInSeconds;
+    public AudioClip mainTick, altTick;
+    private double nextTick = 0.0F;
+    private bool isPlaying = false;
+    private AudioSource audioSource;
+    private int metric = 4, ticksPlayed = 0;
+
+    void Start()
+    {
+        bpmInSeconds = 60 / bpm;
+        nextTick = AudioSettings.dspTime + bpmInSeconds;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        while (AudioSettings.dspTime >= nextTick && isPlaying) {
+
+            RegisterTick();
+            audioSource.Play();
+            nextTick += bpmInSeconds;
+        }
+    }
+
+    private void RegisterTick() {
+        audioSource.clip = (ticksPlayed % metric == 0 ? altTick : mainTick);
+        ticksPlayed++;
+    }
+
+    public void StartMetronome() {
+        isPlaying = true;
+        ticksPlayed = 0;
+    }
+    
+    public void StopMetronome() {
+        isPlaying = false;
+    }
+
+    public int Metric {
+        get { return metric; }
+        set
+        {
+            metric = value;
+            ticksPlayed = 0;
+        }
+    }
+
+    public double BPM {
+        get { return bpm; }
+        set
+        {
+            if (value < 0) bpm = 0;
+            else if (value > 999) bpm = 999;
+            else bpm = value;
+
+            bpmInSeconds = 60 / bpm;
+        }
+    }
+}
