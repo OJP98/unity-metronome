b52747cb60a87fa6b373545033ebca3d5dc3b898
OJP98 <ojuarez_p@hotmail.com>
2021-09-03 17:55:48 -0600

    Generating chord list and adding enumerators

--- Assets/Scripts/Chord.cs
+++ Assets/Scripts/Chord.cs
@@ -1,14 +1,24 @@
-using UnityEngine;
-
 namespace lab_metronomo.Assets.Scripts
 {
+    public enum Function {
+        Tonica, Subdominante, Dominante, None
+    }
+
+    public enum ChordType {
+        Major, Minor, Diminished, Augmented, None
+    }
+
     public class Chord
     {
         public Note[] notes;
         public int range1;
         public int range2;
-        public string chordType;
+        public ChordType chordType;
         public bool isFlat;
+        public int grade;
+        public Function function;
+        public int duration;
+        public bool isStrong;
 
         public Chord(Note[] notes, int range1, int range2)
         {
@@ -25,26 +35,67 @@ namespace lab_metronomo.Assets.Scripts
             }
             this.chordType = GetChordType();
         }
-        public string GetChordType()
+
+        public Chord(int duration, bool isStrong) 
+        {
+            this.duration = duration;
+            this.isStrong = isStrong;
+        }
+
+        public ChordType GetChordType()
         {
             if (range1 == 4 && range2 == 3)
-                return "Major";
+                return ChordType.Major;
             
             if (range1 == 3 && range2 == 4)
-                return "Minor";
+                return ChordType.Minor;
 
             if (range1 == 3 && range2 == 3)
-                return "Diminished";
+                return ChordType.Diminished;
             
             if (range1 == 4 && range2 == 4)
-                return "Augmented";
+                return ChordType.Augmented;
             
-            return "None";
+            return ChordType.None;
+        }
+
+        public Function GetFunction(int grade)
+        {
+            if (grade == 1 || grade == 3 || grade == 6)
+                return Function.Tonica;
+            
+            if (grade == 2 || grade == 4)
+                return Function.Subdominante;
+            
+            if (grade == 5)
+                return Function.Dominante;
+
+            return Function.None;
+        }
+
+        public void AssignPropertiesToLastChord() {
+            this.isStrong = false;
+            this.function = Function.Dominante;
+        }
+
+        public int Grade {
+            get { return grade; }
+            set
+            {
+                grade = value;
+                function = GetFunction(grade);
+            }
+        }
+
+        public void AssignFunction()
+        {
+
         }
 
         public string GetNotes()
         {
             return notes[0].noteName + ", " + notes[1].noteName + ", " + notes[2].noteName;
         }
+
     }
 }
--- Assets/Scripts/ProgressionGeneratorScript.cs
+++ Assets/Scripts/ProgressionGeneratorScript.cs
@@ -7,11 +7,13 @@ public class ProgressionGeneratorScript : MonoBehaviour
 {
     private Utils utils = new Utils();
     private List<int> tiemposCompletos = new List<int>{1, 2, 4};
+    // Para una metrica 3/4, esto debería de ser 2 y 3
     private List<int> tiemposDivididos = new List<int>{2, 4};
     private int CANT_COMPASES = 8;
     private int NEGRAS_POR_COMPAS = 4;
     private int MAX_NEGRAS; 
     private List<int> compaces;
+    private List<Chord> acordes;
 
     void Start() {
         MAX_NEGRAS = CANT_COMPASES * NEGRAS_POR_COMPAS;
@@ -21,29 +23,63 @@ public class ProgressionGeneratorScript : MonoBehaviour
         compaces = SubdividirCompas(compaces);
         Debug.Log(utils.PrintIntList(compaces));
 
+        acordes = GenerateChordList(compaces);
     }
 
-    List<int> SubdividirCompas(List<int> compaces) {
+    private List<int> SubdividirCompas(List<int> compaces) {
         List<int> result = new List<int>();
-        foreach (var compas in compaces)
+
+        foreach (var size in compaces)
         {
-            if (compas > 1)
+            // We don't care about complete times
+            if (size > 1)
             {
-                result.Add(compas * NEGRAS_POR_COMPAS);
+                result.Add(size * NEGRAS_POR_COMPAS);
                 continue;
             }
-            // 50 - 50 separamos o no
-            int separar = Random.Range(0, 2);
-            if (separar == 0)
+            // 50 - 50 should we split?
+            int split = Random.Range(0, 2);
+            if (split == 0)
             {
-                result.Add(compas * NEGRAS_POR_COMPAS);
+                result.Add(size * NEGRAS_POR_COMPAS);
                 continue;
             }
 
+            // Decide wether we devide in 2 o 4
             int randomIndex = Random.Range(0, 2);
-            int subdivisiones = tiemposDivididos[randomIndex];
-            result.AddRange(Enumerable.Repeat((subdivisiones == 4 ? 1 : 2), subdivisiones));
+            int subdivisions = tiemposDivididos[randomIndex];
+            // Add the number n times
+            result.AddRange(
+                Enumerable.Repeat((subdivisions == 4 ? 1 : 2), subdivisions)
+            );
         }
+        return result;
+    }
+
+    private List<Chord> GenerateChordList(List<int> durationList) {
+        List<Chord> result = new List<Chord>();
+        int listCount = durationList.Count;
+        int prevDuration = 1;
+        bool prevIsStrong = false;
+
+        for (int i = 0; i < listCount; i++)
+        {
+            int duration = durationList[i];
+            bool isStrong = true;
+
+            // Is the last chord duration same as this one?
+            if (duration % prevDuration == 0 && prevIsStrong)
+                isStrong = false;
+
+            // Create a new chord and add it to the list
+            Chord chord = new Chord(duration, isStrong);
+            result.Add(chord);
+
+            // Save the previous values
+            prevDuration = duration;
+            prevIsStrong = isStrong;
+        }
+        result[listCount - 1].AssignPropertiesToLastChord();
         return result;
     }
 }
