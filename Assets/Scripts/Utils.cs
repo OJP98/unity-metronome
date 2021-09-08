fd6be747c93f85759d4d754e5bc0b418ec572d6c
OJP98 <ojuarez_p@hotmail.com>
2021-09-07 23:38:49 -0600

    Better variable names

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -6,14 +6,17 @@ namespace lab_metronomo.Assets.Scripts
 {
   public class Utils
     {
-        public string[] noteNames = {"do", "do#", "re", "re#", "mi", "fa", "fa#", "sol", "sol#", "la", "la#", "si"};
+        private int TOTAL_NOTES;
+        public string[] NOTE_NAMES = {"do", "do#", "re", "re#", "mi", "fa", "fa#", "sol", "sol#", "la", "la#", "si"};
         public string[] majorScale;
         public int initialNote;
+
 
         public Utils(int initialNote = 0)
         {
             this.initialNote = initialNote;
             this.majorScale = GetMajorScaleFromInitialNote(this.initialNote);
+            this.TOTAL_NOTES = this.NOTE_NAMES.Length;
         }
 
         public List<int> GenerateRandomKey(int maxValue, List<int> randomValues)
@@ -43,13 +46,13 @@ namespace lab_metronomo.Assets.Scripts
         public string[] GetMajorScaleFromInitialNote(int index)
         {
             string[] resNotes = {
-                noteNames[index],
-                noteNames[(index+2)%12],
-                noteNames[(index+4)%12],
-                noteNames[(index+5)%12],
-                noteNames[(index+7)%12],
-                noteNames[(index+9)%12],
-                noteNames[(index+11)%12]
+                NOTE_NAMES[index],
+                NOTE_NAMES[(index+2) % TOTAL_NOTES],
+                NOTE_NAMES[(index+4) % TOTAL_NOTES],
+                NOTE_NAMES[(index+5) % TOTAL_NOTES],
+                NOTE_NAMES[(index+7) % TOTAL_NOTES],
+                NOTE_NAMES[(index+9) % TOTAL_NOTES],
+                NOTE_NAMES[(index+11) % TOTAL_NOTES]
             };
 
             return resNotes;
@@ -58,21 +61,21 @@ namespace lab_metronomo.Assets.Scripts
         public Note[] GetChordFromGrade(int grade) 
         {
             grade -= 1;
-            string note1 = majorScale[grade];
-            string note2 = majorScale[(grade+2)%7];
-            string note3 = majorScale[(grade+4)%7];
+            string noteName1 = majorScale[grade];
+            string noteName2 = majorScale[(grade+2)%7];
+            string noteName3 = majorScale[(grade+4)%7];
 
-           int noteIndex1 = Array.IndexOf(noteNames, note1);
-           int noteIndex2 = Array.IndexOf(noteNames, note2);
-           int noteIndex3 = Array.IndexOf(noteNames, note3);
+           int noteIndex1 = Array.IndexOf(NOTE_NAMES, noteName1);
+           int noteIndex2 = Array.IndexOf(NOTE_NAMES, noteName2);
+           int noteIndex3 = Array.IndexOf(NOTE_NAMES, noteName3);
 
-           if (noteIndex1 > noteIndex2) noteIndex2 += 12;
-           if (noteIndex2 > noteIndex3) noteIndex3 += 13;
+           if (noteIndex1 > noteIndex2) noteIndex2 += TOTAL_NOTES;
+           if (noteIndex2 > noteIndex3) noteIndex3 += TOTAL_NOTES;
 
            return new Note[] {
-               new Note(note1, noteIndex1),
-               new Note(note2, noteIndex2),
-               new Note(note3, noteIndex3)
+               new Note(noteName1, noteIndex1),
+               new Note(noteName2, noteIndex2),
+               new Note(noteName3, noteIndex3)
            };
         }
     }
